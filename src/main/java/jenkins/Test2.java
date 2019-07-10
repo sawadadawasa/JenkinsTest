a255bf06905bacd27d470a09b1ef72f8597c0177
shogo.sawada <shogo.sawada@rakus-partners.co.jp>
2019-07-10 09:39:25 +0900

    num

---
+++ src/main/java/jenkins/Test2.java
@@ -0,0 +1,16 @@
+package jenkins;
+
+public abstract class Test2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		int num1 = 1;
+		int num2 = 2;
+		
+		System.out.println(num1 + num2);	
+		System.out.println(num1 * num2);
+
+	}
+
+}
